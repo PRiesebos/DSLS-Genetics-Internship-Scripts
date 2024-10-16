e2c8edcbefb65b54a2ea6a1482243f7686d7f837
Roy Oelen <roy.oelen@gmail.com>
2024-10-16 18:03:19 +0200

    - wrong module name for picardtool

--- 1000IBD/gem_liftover_1000IBD_variants.sh
+++ 1000IBD/gem_liftover_1000IBD_variants.sh
@@ -8,11 +8,10 @@
 ###################################################################
 
 
-
 # load libraries
 ml HTSlib
 ml BCFtools
-ml picardtools
+ml picard
 
 # rezip the vcf to have bgzip compression
 gunzip -c european_GRCh37_maf0001.vcf.gz | bgzip > european_GRCh37_maf0001.bgz.vcf.gz
@@ -23,7 +22,7 @@ java -jar -Xms60g -Xmx60g \
       -R /groups/umcg-franke-scrna/tmp04/external_datasets/hg38/ref_genome_QC/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
       -O /groups/umcg-franke-scrna/tmp04/external_datasets/hg38/ref_genome_QC/Homo_sapiens.GRCh38.dna.primary_assembly.dict
 
-# subset each the vcf for each chromosome, as liftover of one chromosome was too memory-intensive
+# subset each the vcf for each chromosome, as liftover of all chromosomes together was too memory-intensive
 for i in {1..22}
     do
         bcftools view /groups/umcg-fg/tmp04/projects/gut-bulk/ongoing/2024-02-07-GutPublicRNASeq/datasets/Werna/paper_genotype/european_GRCh37_maf0001.bgz.vcf.gz --regions ${i} -o /groups/umcg-fg/tmp04/projects/gut-bulk/ongoing/2024-02-07-GutPublicRNASeq/datasets/Werna/paper_genotype/european_GRCh37_maf0001_chr${i}.vcf.gz -Oz
